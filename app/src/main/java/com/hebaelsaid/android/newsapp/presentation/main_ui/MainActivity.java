3bd0129b3e5998ee20c9cb8d79d4ad720611af00
Heba Elsaid <hebaelsaid912@gmail.com>
2022-11-05 13:02:29 +0200

    add project packages structure

--- app/src/main/java/com/hebaelsaid/android/newsapp/presentation/main_ui/MainActivity.java
+++ app/src/main/java/com/hebaelsaid/android/newsapp/presentation/main_ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.hebaelsaid.android.newsapp;
+package com.hebaelsaid.android.newsapp.presentation.main_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.hebaelsaid.android.newsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
