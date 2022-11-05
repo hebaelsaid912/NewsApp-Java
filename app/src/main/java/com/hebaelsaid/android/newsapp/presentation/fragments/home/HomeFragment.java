dfe109032a168ce4bd4b9189e89f1014b74bbab6
Heba Elsaid <hebaelsaid912@gmail.com>
2022-11-05 13:53:10 +0200

    handle homeFragment main design

---
+++ app/src/main/java/com/hebaelsaid/android/newsapp/presentation/fragments/home/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.hebaelsaid.android.newsapp.presentation.fragments.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hebaelsaid.android.newsapp.R;
+
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
--- app/src/main/java/com/hebaelsaid/android/newsapp/presentation/main_ui/MainActivity.java
+++ app/src/main/java/com/hebaelsaid/android/newsapp/presentation/main_ui/MainActivity.java
@@ -1,16 +1,19 @@
 package com.hebaelsaid.android.newsapp.presentation.main_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.hebaelsaid.android.newsapp.R;
+import com.hebaelsaid.android.newsapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 }
