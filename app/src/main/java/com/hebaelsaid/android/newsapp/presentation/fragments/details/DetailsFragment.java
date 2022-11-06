0626bbe7ac626a029eb719ae7d260ad5b5839085
Heba Elsaid <hebaelsaid912@gmail.com>
2022-11-06 19:44:18 +0200

    handle backing from details screen

--- app/src/main/java/com/hebaelsaid/android/newsapp/presentation/fragments/details/DetailsFragment.java
+++ app/src/main/java/com/hebaelsaid/android/newsapp/presentation/fragments/details/DetailsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,5 +33,14 @@ public class DetailsFragment extends Fragment {
         return fragmentDetailsBinding.getRoot();
     }
 
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        fragmentDetailsBinding.newsDetailsBackImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigateUp();
+            }
+        });
+    }
 }
