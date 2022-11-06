a379a20d8e1439dd8bcff0831d1177e58edf0419
Heba Elsaid <hebaelsaid912@gmail.com>
2022-11-06 14:48:57 +0200

    handle connection between bottomNavBar and navigation graph

---
+++ app/src/main/java/com/hebaelsaid/android/newsapp/presentation/fragments/home/search/SearchFragment.java
@@ -0,0 +1,31 @@
+package com.hebaelsaid.android.newsapp.presentation.fragments.home.search;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hebaelsaid.android.newsapp.R;
+
+public class SearchFragment extends Fragment {
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
--- app/src/main/java/com/hebaelsaid/android/newsapp/presentation/main_ui/MainActivity.java
+++ app/src/main/java/com/hebaelsaid/android/newsapp/presentation/main_ui/MainActivity.java
@@ -2,6 +2,10 @@ package com.hebaelsaid.android.newsapp.presentation.main_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -10,9 +14,15 @@ import com.hebaelsaid.android.newsapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.main_app_constraint_Fragment_layout);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(mainBinding.bottomNav, navController);
     }
 }
